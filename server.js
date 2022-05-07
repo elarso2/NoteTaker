1461be8de34a04b843cfcdcb8e10f6f1f57c2a00
Elizabeth Larson <elarso2@uw.edu>
2022-05-06 19:37:14 -0700

    fixing file paths, scope of fs functions, writing to file and saving on screen now functioning

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 const res = require("express/lib/response");
+const { fstat } = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const util = require("util");
+const db = require("./db/db.json");
 
 const PORT = process.env.port || 3001;
 // const api = require("./public/assets/js/index");
@@ -21,9 +25,11 @@ app.get("/notes", (req, res) =>
 app.get("/notes", (req, res) => res.status(200).json(notes));
 
 // GET route /api/notes to read the db.json file and return saved notes as JSON
-// app.get("/api/notes", (req,res) =>
-
-// );
+app.get("/api/notes", (req, res) => {
+  const readFromFile = util.promisify(fs.readFile);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+  console.log(`${req.method} request received`);
+});
 
 app.get("api/notes", (req, res) => res.status(200).json(db));
 
@@ -35,6 +41,17 @@ app.post("/api/notes", (req, res) => {
     text,
     id: uuidv4(),
   };
+
+  const response = {
+    status: "success",
+    body: newNote,
+  };
+
+  db.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(db));
+  console.log(`${req.method} request received`);
+
+  res.status(200).json(response);
 });
 
 // GET route * to return to index.html
