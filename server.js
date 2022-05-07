2a085a2b7d0652c34e5fea70b6cacdd48cea516e
Elizabeth Larson <elarso2@uw.edu>
2022-05-06 20:11:23 -0700

    changing port

--- server.js
+++ server.js
@@ -59,6 +59,4 @@ app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => console.log(`App listening at ${PORT} 🚀`));
