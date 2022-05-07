eda779e73907de4a68ca3e5425e0ec044b51c859
Elizabeth Larson <elarso2@uw.edu>
2022-05-06 18:02:25 -0700

    adding port, connecting note file path, portions of other get and post route code

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const res = require("express/lib/response");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
+const PORT = process.env.port || 3001;
+// const api = require("./public/assets/js/index");
+// app.use("/api", api);
 const app = express();
 
 app.use(express.json());
@@ -14,14 +18,30 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// GET route * to return to index.html
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+app.get("/notes", (req, res) => res.status(200).json(notes));
 
 // GET route /api/notes to read the db.json file and return saved notes as JSON
 // app.get("/api/notes", (req,res) =>
 
 // );
 
+app.get("api/notes", (req, res) => res.status(200).json(db));
+
 // POST route /api/notes to receive new note, add to db.json, and return new note to client
+app.post("/api/notes", (req, res) => {
+  const { title, text, id } = req.body;
+  const newNote = {
+    title,
+    text,
+    id: uuidv4(),
+  };
+});
+
+// GET route * to return to index.html
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
